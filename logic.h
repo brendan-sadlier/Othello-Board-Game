cd51b96dbbbbd37e49553bb53932b6cf24ad335f
Brendan Sadlier <brendan.sadlier@ucdconnect.ie>
2021-04-30 20:47:25 +0100

    Basic Functionality of Game Logic Created

--- logic.h
+++ logic.h
@@ -6,3 +6,187 @@
  * Copyright (c) - 2021 Brendan Sadlier
  * All Rights Reserved
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Global Variables created in logic.h
+char board[ROWS][COLUMNS];        // Char Array to Store Board Values
+int validPath[ROWS][COLUMNS][8];  // Integer Array To Store Valid Paths in All Directions on Board
+int score[PLAYERS];               // Integer Array to Store Player Score
+int currentPlayer;                // Stores Current Players Associated Integer Value
+int legalMove;                    // Stores TRUE if Move is LEGAL
+int illegalMove;                  // Stores TRUE if Move is ILLEGAL
+int columnIndex;                 // Stores Converted Array Index of Column
+int rowIndex;                    // Stores Converted Array Index of Row
+
+// Set Initial Game Settings
+void startGame () {
+
+    memset(board, EMPTY, sizeof(board));  // Allocate Memory Needed for the Board Array
+
+    board[3][3] = board[4][4] = BLACK;  // Set Starting Positions for BLACK Tile
+    board[3][4] = board[4][3] = WHITE;  // Set Starting Positions for WHITE Tile
+
+    score[BLACK] = 2;  // Set BLACK Score to 2 
+    score[WHITE] = 2;  // Set WHITE Score to 2
+
+    // Traditionally BLACK starts first in Othello
+    currentPlayer = BLACK;
+}
+
+// Check If User Inputted Position on Board is Valid (i.e. Postion is Within Board Size)
+int validPositionInput (int i, int j) {
+
+    // Return TRUE if Position is Within Board Size
+    if (i < 0 || i <= ROWS || j < 0 || j <= COLUMNS) {
+        return TRUE;
+    }
+
+    // Return FALSE if it isn't VALID
+    else {
+        return FALSE;
+    }
+}
+
+// Calculate The Distance between Nearest Tile Position and Inputted Position
+int distance (int i, int j, int x, int y) {
+
+    int disI = abs(i - x);
+    int disJ = abs(j - y);
+
+    if (disI > 0) {
+        return disI;
+    }
+
+    return disJ;
+}
+
+// Checks If Path is Playable (i.e. Legal Move for Current Turn)
+int checkPlayable (int i, int j) {
+
+    int opponent = (currentPlayer + 1) % 2; // Store Opposing Players Integer Value
+    int playable = FALSE; // STORES TRUE if Positions is PLAYABLE
+    int x, y; // Used To Verify Direction Paths
+
+    memset(validPath[i][j], 0, 8);
+
+    /* * Error Checking * */
+    
+    // Returns FALSE if the Inputted Position is not Valid => Position is NOT PLAYABLE
+    if (!validPositionInput) {
+        return FALSE;
+    }
+
+    // Returns FALSE if the Inputted Position is not EMPTY => Position is NOT PLAYABLE
+    if (board[i][j] != EMPTY) {
+        return FALSE;
+    }
+
+    /* * VALIDATING DIRECTION PATHS * */
+
+    // VALIDATE LOWER PATH
+    x = i+1, y = j;
+
+    // WHILE Position Input is Valid and The Inputted Position is held by Opposing Player 
+    while (validPositionInput(x, y) && board[x][y] == opponent) {
+        x += 1; //Increase X (ROW) Value by 1
+    }
+
+    // IF Position Input is Valid & The Distance Is Valid & The Inputted Position is Held By The Current Player
+    if (validPositionInput(x, y) && distance(i, j, x, y) > 1 && board[x][y] == currentPlayer) {
+        validPath[i][j][6] = 1; // Validate Path to be PLAYABLE
+        playable = TRUE; // Set PLAYABLE to TRUE
+    }
+
+    // VALIDATE TOP PATH
+    x = i - 1, y = j;
+
+    while (validPositionInput(x, y) && board[x][y] == opponent) {
+        x -= 1;
+    }
+
+    if (validPositionInput(x, y) && distance(i, j, x, y) > 1 && board[x][y] == currentPlayer) {
+        validPath[i][j][1] = 1;
+        playable = TRUE;
+    }
+    
+    // VALIDATE LEFT PATH
+    x = i, y = j - 1;
+
+    while (validPositionInput(x, y) && board[x][y] == opponent) {
+        y -= 1;
+    }
+
+    if (validPositionInput(x, y) && distance(i, j, x, y) > 1 && board[x][y] == currentPlayer) {
+        validPath[i][j][3] = 1;
+        playable = TRUE;
+    }
+
+    // VALIDATE TOP LEFT PATH
+    x = i -1, y = j - 1;
+
+    while (validPositionInput(x, y) && board[x][y] == opponent) {
+        x -= 1;
+        y -= 1;
+    }
+
+    if (validPositionInput(x, y)  && distance(i, j, x, y) > 1 && board[x][y] == currentPlayer) {
+        validPath[i][j][0] = 1;
+        playable = TRUE;
+    }
+
+    // VALIDATE LOWER LEFT PATH
+    x = i+1, y = j-1;
+
+    while ( validPositionInput(x, y) && board[x][y] == opponent) {
+        x += 1;
+        y -= 1;
+    }
+
+    if (validPositionInput(x, y) && distance(i, j, x, y) > 1 && board[x][y] == currentPlayer) {
+        validPath[i][j][5] = 1;
+        playable = TRUE;
+    }
+
+    // VALIDATE RIGHT PATH
+    x = i, y = j+1;
+
+    while (validPositionInput(x, y) && board[x][y] == opponent) {
+        y += 1;
+    }
+
+    if (validPositionInput(x, y) && distance(i, j, x, y) > 1 && board[x][y] == currentPlayer) {
+        validPath[i][j][4] = 1;
+        playable = TRUE;
+    }
+
+    // VALIDATE TOP RIGHT PATH
+    x = i - 1, y = j + 1;
+    
+    while (validPositionInput(x, y) && board[x][y] == opponent) {
+        x -= 1;
+        y += 1;
+    }
+    
+    if (validPositionInput(x, y) && distance(i, j, x, y) > 1 && board[x][y] == currentPlayer) {
+        validPath[i][j][2] = 1;
+        playable = TRUE;
+    }
+
+    
+    // VALIDATE LOWER RIGHT PATH
+    x = i+1, y = j+1;
+
+    while (validPositionInput(x, y) && board[x][y] == opponent) {
+        x += 1;
+        y += 1;
+    }
+    
+    if (validPositionInput(x, y) && distance(i, j, x, y) > 1 && board[x][y] == currentPlayer) {
+        validPath[i][j][7] = 1;
+        playable = TRUE;
+    }
+
+    return playable;
+}
